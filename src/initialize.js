b41ef45ed670208074ce512739f125cedabf34af
Max Gruenfelder <maximilian.gruenfelder@sap.com>
2023-06-20 13:00:29 +0200

    test prettier

--- src/initialize.js
+++ src/initialize.js
@@ -19,7 +19,9 @@ const readFileAsync = promisify(fs.readFile);
 
 const VERROR_CLUSTER_NAME = "EventQueueInitialization";
 const COMPONENT = "eventQueue/initialize";
-const BASE_TABLES = { EVENT: "sap.eventqueue.Event", LOCK: "sap.eventqueue.Lock",
+const BASE_TABLES = {
+  EVENT: "sap.eventqueue.Event",
+  LOCK: "sap.eventqueue.Lock",
 };
 
 const initialize = async ({
