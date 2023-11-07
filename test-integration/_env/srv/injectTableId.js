3d57aa348bf62f9649690a0e488f817b75ae8d12
Max Gruenfelder <maximilian.gruenfelder@sap.com>
2023-11-07 14:57:14 +0100

    wip

--- test-integration/_env/srv/injectTableId.js
+++ test-integration/_env/srv/injectTableId.js
@@ -1,13 +1,11 @@
 "use strict";
 
-(async () => {
-  const tableId = process.env.TABLE_GUID;
-  if (!tableId) {
-    // eslint-disable-next-line no-console
-    console.error("missing tableId in env section");
-    process.exit(-1);
-  }
+const tableId = process.env.TABLE_GUID;
+if (!tableId) {
   // eslint-disable-next-line no-console
-  console.log("table id", tableId);
-  process.exit(1);
-})();
+  console.error("missing tableId in env section");
+  process.exit(-1);
+}
+// eslint-disable-next-line no-console
+console.log("table id", tableId);
+process.exit(1);
