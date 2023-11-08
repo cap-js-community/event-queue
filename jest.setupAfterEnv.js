d2a2120e964cd2642de6bae0b141594d907952e2
Max Gruenfelder <maximilian.gruenfelder@sap.com>
2023-11-08 13:17:45 +0100

    wip

--- jest.setupAfterEnv.js
+++ jest.setupAfterEnv.js
@@ -1,9 +1,9 @@
 "use strict";
 
-// turn off regular and error logging;
-jest.spyOn(console, "log").mockImplementation();
-jest.spyOn(console, "info").mockImplementation();
-jest.spyOn(console, "warn").mockImplementation();
-jest.spyOn(console, "error").mockImplementation();
-jest.spyOn(process.stdout, "write").mockImplementation();
-jest.spyOn(process.stderr, "write").mockImplementation();
+// // turn off regular and error logging;
+// jest.spyOn(console, "log").mockImplementation();
+// jest.spyOn(console, "info").mockImplementation();
+// jest.spyOn(console, "warn").mockImplementation();
+// jest.spyOn(console, "error").mockImplementation();
+// jest.spyOn(process.stdout, "write").mockImplementation();
+// jest.spyOn(process.stderr, "write").mockImplementation();
--- test-integration/_env/srv/server.js
+++ test-integration/_env/srv/server.js
@@ -5,15 +5,16 @@ const { generateCredentialsForCds } = require("./hana/helper");
 
 let credentials = JSON.parse(process.env.HANA_DB_CREDENTIALS || null);
 try {
-  if (process.env.NODE_ENV === "local-hana") {
-    credentials = require("../db/default-env").VCAP_SERVICES.hana[0].credentials;
-  } else if (process.env.NODE_ENV === "githubAction-hana") {
+  if (process.env.NODE_ENV === "githubAction-hana") {
     credentials = generateCredentialsForCds();
+  } else {
+    credentials = require("../db/default-env").VCAP_SERVICES.hana[0].credentials;
   }
 } catch {
   // Nothing to do
 }
 
+cds.log("/server").info("running on hana schema: ", credentials.schema);
 cds.env.requires.db = {
   kind: "hana",
   credentials,
