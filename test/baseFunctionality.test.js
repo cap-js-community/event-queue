2d3217968387f71ae994bfcd61f36afc840c3289
Max Gruenfelder <maximilian.gruenfelder@sap.com>
2023-11-09 13:35:06 +0100

    wip testing

--- test/baseFunctionality.test.js
+++ test/baseFunctionality.test.js
@@ -92,6 +92,14 @@ describe("baseFunctionality", () => {
     await testHelper.selectEventQueueAndExpectOpen(tx);
   });
 
+  test("insert one delayed entry in past - should be directly be processed", async () => {
+    await testHelper.insertEventEntry(tx, { delayedSeconds: -100 });
+    const event = eventQueue.getConfigInstance().events[0];
+    await eventQueue.processEventQueue(context, event.type, event.subType);
+    expect(loggerMock.callsLengths().error).toEqual(0);
+    await testHelper.selectEventQueueAndExpectDone(tx);
+  });
+
   test("should do nothing if no lock is available", async () => {
     await testHelper.insertEventEntry(tx);
     jest.spyOn(eventQueue.EventQueueProcessorBase.prototype, "handleDistributedLock").mockResolvedValueOnce(false);
