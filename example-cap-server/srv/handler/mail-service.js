177ca1278f706b5157e45bd69a1ee0fedabc264f
Max Gruenfelder <maximilian.gruenfelder@sap.com>
2024-01-18 10:14:46 +0100

    wip

--- example-cap-server/srv/handler/mail-service.js
+++ example-cap-server/srv/handler/mail-service.js
@@ -10,18 +10,6 @@ const single = async (context) => {
     payload: JSON.stringify(context.data),
     ...(context.data.startAfter && { startAfter: new Date(Date.now() + context.data.startAfter * 1000).toISOString() }),
   });
-  const audit = await cds.connect.to("audit-log");
-  const crypto = await cds.connect.to("CryptoService");
-  const mail = await cds.connect.to("MailService");
-  const cryptoOutbox = cds.outboxed(crypto);
-  const t1 = await cryptoOutbox.send("hash", {
-    duration: 10,
-  });
-  const t2 = await cryptoOutbox.emit("hash", {
-    duration: 10,
-  });
-
-  debugger;
 };
 
 const cluster = async (context) => {
