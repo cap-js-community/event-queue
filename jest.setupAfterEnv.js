934068a974b0656e0858268d5e3112e584fb342c
Max Gruenfelder <maximilian.gruenfelder@sap.com>
2024-02-01 13:48:54 +0100

    wip

--- jest.setupAfterEnv.js
+++ jest.setupAfterEnv.js
@@ -1,11 +1,23 @@
 "use strict";
 
-process.env.CDS_CONFIG = '{ "requires": { "outbox": "persistent-outbox" } }';
+const parsedCdsOptions = JSON.parse(process.env.CDS_CONFIG ?? "{}");
+
+parsedCdsOptions.requires ??= {};
+parsedCdsOptions.requires.outbox = "persistent-outbox";
+if (!process.env.NEW_DB_SERVICE) {
+  parsedCdsOptions.requires.db = {
+    kind: "legacy-sqlite",
+    credentials: {
+      url: ":memory:",
+    },
+  };
+}
+process.env.CDS_CONFIG = JSON.stringify(parsedCdsOptions);
 
 // turn off regular and error logging;
-// jest.spyOn(console, "log").mockImplementation();
-// jest.spyOn(console, "info").mockImplementation();
+jest.spyOn(console, "log").mockImplementation();
+jest.spyOn(console, "info").mockImplementation();
 jest.spyOn(console, "warn").mockImplementation();
 jest.spyOn(console, "error").mockImplementation();
-// jest.spyOn(process.stdout, "write").mockImplementation();
-// jest.spyOn(process.stderr, "write").mockImplementation();
+jest.spyOn(process.stdout, "write").mockImplementation();
+jest.spyOn(process.stderr, "write").mockImplementation();
