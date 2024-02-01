81300ff094df2cb32a0f87f96c41e5372408ad80
Max Gruenfelder <max.grunfelder@gmail.com>
2024-02-01 22:51:16 +0100

    Add test for releasing lock periodic event (#116)

--- src/processEventQueue.js
+++ src/processEventQueue.js
@@ -116,9 +116,6 @@ const reevaluateShouldContinue = (eventTypeInstance, iterationCounter, startTime
 };
 
 const processPeriodicEvent = async (eventTypeInstance) => {
-  let queueEntry;
-  let processNext = true;
-
   const isPeriodicEventBlockedCb = config.isPeriodicEventBlockedCb;
   const params = [eventTypeInstance.eventType, eventTypeInstance.eventSubType, eventTypeInstance.context.tenant];
   let eventBlocked = false;
@@ -145,6 +142,8 @@ const processPeriodicEvent = async (eventTypeInstance) => {
   }
 
   try {
+    let queueEntry;
+    let processNext = true;
     while (processNext) {
       await executeInNewTransaction(
         eventTypeInstance.context,
