85a82728fe39104ecec49abbf7fd9eb6e3b4ea1b
Max Gruenfelder <maximilian.gruenfelder@sap.com>
2024-02-05 21:21:34 +0100

    wip

--- example-cap-server/srv/facade/task-service.js
+++ example-cap-server/srv/facade/task-service.js
@@ -14,6 +14,12 @@ module.exports = class TaskService extends cds.Service {
         ID: req.data.ID,
       });
       await promisify(setTimeout)(15 * 1000);
+      const mailService = await cds.connect.to("MailService");
+      const task = await SELECT.one.from("sap.eventqueue.sample.Task").where({ ID: req.data.ID });
+      await mailService.tx(req).sendSingle({
+        to: "dummy@mail.com",
+        subject: `Processing of task: '${task.description}' done.`,
+      });
       await UPDATE.entity("sap.eventqueue.sample.Task").set({ status: "done" }).where({ ID: req.data.ID });
       logger.info("task processed", {
         ID: req.data.ID,
