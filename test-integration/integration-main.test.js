fb05f028661b947345d6d56ce7d49b47bdea5bc1
Max Gruenfelder <max.grunfelder@gmail.com>
2024-02-22 16:19:47 +0100

    New cap db services (#98)
    * use cap db services
    
    * use cap db services
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * linter
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * upgrade-lock
    
    * wip
    
    * skip for new db service
    
    * wip
    
    * wip
    
    ---------
    
    Co-authored-by: Max Gruenfelder <maximilian.gruenfelder@sap.com>

--- test-integration/integration-main.test.js
+++ test-integration/integration-main.test.js
@@ -251,7 +251,8 @@ describe("integration-main", () => {
     eventQueue.config.dbUser = null;
   });
 
-  it("lock wait timeout during keepAlive", async () => {
+  const onlyOldDbService = process.env.NEW_DB_SERVICE ? it.skip : it;
+  onlyOldDbService("lock wait timeout during keepAlive", async () => {
     await cds.tx({}, (tx2) => testHelper.insertEventEntry(tx2));
     dbCounts = {};
     const event = eventQueue.config.events[0];
@@ -274,8 +275,10 @@ describe("integration-main", () => {
       });
     await eventQueue.processEventQueue(context, event.type, event.subType);
     doCheck = false;
+    // eslint-disable-next-line jest/no-standalone-expect
     expect(loggerMock.callsLengths().error).toEqual(1);
     await testHelper.selectEventQueueAndExpectError(tx);
+    // eslint-disable-next-line jest/no-standalone-expect
     expect(dbCounts).toMatchSnapshot();
   });
 
