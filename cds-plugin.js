61e948fc5b108787d69423359fb16da3cf2c04dc
Max Gruenfelder <maximilian.gruenfelder@sap.com>
2024-02-23 17:04:01 +0100

    smaller cleanup and fix cds plugin setup

--- cds-plugin.js
+++ cds-plugin.js
@@ -5,6 +5,6 @@ const cds = require("@sap/cds");
 const eventQueue = require("./src");
 const COMPONENT_NAME = "/eventQueue/plugin";
 
-if (!cds.build.register) {
+if (!cds.build.register && cds.env.eventQueue) {
   eventQueue.initialize().catch((err) => cds.log(COMPONENT_NAME).error(err));
 }
