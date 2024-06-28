45d8b841411e656a2612bbe02d3974d2b851f8e7
Max Gruenfelder <maximilian.gruenfelder@sap.com>
2024-06-28 16:43:27 +0200

    plugin

--- cds-plugin.js
+++ cds-plugin.js
@@ -5,6 +5,6 @@ const cds = require("@sap/cds");
 const eventQueue = require("./src");
 const COMPONENT_NAME = "/eventQueue/plugin";
 
-if (!cds.build?.register && cds.env.eventQueue) {
+if (!cds.build?.register && Object.keys(cds.env.eventQueue ?? {}).length) {
   module.exports = eventQueue.initialize().catch((err) => cds.log(COMPONENT_NAME).error(err));
 }
