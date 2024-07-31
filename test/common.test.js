8ff4105a6cf375b7f002c9011107bbf9f57eeced
Max Gruenfelder <maximilian.gruenfelder@sap.com>
2024-07-31 11:01:48 +0200

    fix xssec

--- test/common.test.js
+++ test/common.test.js
@@ -33,7 +33,9 @@ describe("getAuthInfo", () => {
   });
 
   it("should set and return new AuthInfo", async () => {
-    jest.spyOn(xssec.XsuaaService.prototype, "fetchClientCredentialsToken").mockResolvedValueOnce("token");
+    jest
+      .spyOn(xssec.XsuaaService.prototype, "fetchClientCredentialsToken")
+      .mockResolvedValueOnce({ access_token: "token" });
     jest.spyOn(xssec.XsuaaService.prototype, "createSecurityContext").mockResolvedValueOnce({
       getExpirationDate: () => new Date(),
     });
@@ -43,7 +45,9 @@ describe("getAuthInfo", () => {
   });
 
   it("should use cache for the second call", async () => {
-    jest.spyOn(xssec.XsuaaService.prototype, "fetchClientCredentialsToken").mockResolvedValueOnce("token");
+    jest
+      .spyOn(xssec.XsuaaService.prototype, "fetchClientCredentialsToken")
+      .mockResolvedValueOnce({ access_token: "token" });
     jest.spyOn(xssec.XsuaaService.prototype, "createSecurityContext").mockResolvedValueOnce({
       getExpirationDate: () => new Date(Date.now() + 61 * 1000),
     });
@@ -61,7 +65,7 @@ describe("getAuthInfo", () => {
 
   it("should use cache for parallel calls", async () => {
     jest.spyOn(xssec.XsuaaService.prototype, "fetchClientCredentialsToken").mockImplementationOnce(() => {
-      return new Promise((resolve) => setTimeout(resolve, 5));
+      return new Promise((resolve) => setTimeout(() => resolve({ access_token: "token" }), 5));
     });
     jest.spyOn(xssec.XsuaaService.prototype, "createSecurityContext").mockResolvedValueOnce({
       getExpirationDate: () => new Date(Date.now() + 65 * 1000),
@@ -79,7 +83,9 @@ describe("getAuthInfo", () => {
   });
 
   it("should not use cache if validity is below 60 seconds (margin)", async () => {
-    jest.spyOn(xssec.XsuaaService.prototype, "fetchClientCredentialsToken").mockResolvedValue("token");
+    jest
+      .spyOn(xssec.XsuaaService.prototype, "fetchClientCredentialsToken")
+      .mockResolvedValue({ access_token: "token" });
     jest.spyOn(xssec.XsuaaService.prototype, "createSecurityContext").mockImplementation(async () => {
       return {
         getExpirationDate: () => new Date(Date.now() + 59 * 1000),
@@ -97,7 +103,9 @@ describe("getAuthInfo", () => {
   });
 
   it("should not use cache for different tenants", async () => {
-    jest.spyOn(xssec.XsuaaService.prototype, "fetchClientCredentialsToken").mockResolvedValue("token");
+    jest
+      .spyOn(xssec.XsuaaService.prototype, "fetchClientCredentialsToken")
+      .mockResolvedValue({ access_token: "token" });
     jest.spyOn(xssec.XsuaaService.prototype, "createSecurityContext").mockImplementation(async () => {
       return {
         getExpirationDate: () => new Date(Date.now() + 120 * 1000),
@@ -127,7 +135,9 @@ describe("getAuthInfo", () => {
     expect(result).toBeUndefined();
     expect(cds.log().warn.mock.calls).toMatchSnapshot();
 
-    jest.spyOn(xssec.XsuaaService.prototype, "fetchClientCredentialsToken").mockResolvedValue("token");
+    jest
+      .spyOn(xssec.XsuaaService.prototype, "fetchClientCredentialsToken")
+      .mockResolvedValue({ access_token: "token" });
     jest.spyOn(xssec.XsuaaService.prototype, "createSecurityContext").mockImplementation(async () => {
       return {
         getExpirationDate: () => new Date(Date.now() + 120 * 1000),
