7d5ee5233d009c13e170412c1b76379246586484
Max Gruenfelder <maximilian.gruenfelder@sap.com>
2024-10-18 17:54:52 +0200

    fix tests

--- test/calculateFutureDate.test.js
+++ test/calculateFutureDate.test.js
@@ -51,4 +51,40 @@ describe("calculateFutureDate function", () => {
     const result = calculateFutureDate(interval, desiredTime, true);
     expect(result.toISOString()).toEqual("2024-10-17T15:00:00.000Z");
   });
+
+  test("Should also work for long intervals - desired shortly after current time", () => {
+    const now = new Date("2024-10-18T14:00:00.000Z");
+    jest.setSystemTime(now);
+    const interval = 7 * 24 * 60 * 60; // 7 day
+    const desiredTime = "15:00";
+    const result = calculateFutureDate(interval, desiredTime, true);
+    expect(result.toISOString()).toEqual("2024-10-11T15:00:00.000Z");
+  });
+
+  test("Should also work for long intervals - desired shortly before current time", () => {
+    const now = new Date("2024-10-18T14:00:00.000Z");
+    jest.setSystemTime(now);
+    const interval = 7 * 24 * 60 * 60; // 7 day
+    const desiredTime = "13:00";
+    const result = calculateFutureDate(interval, desiredTime, true);
+    expect(result.toISOString()).toEqual("2024-10-18T13:00:00.000Z");
+  });
+
+  test("Current time late night and desired time next morning", () => {
+    const now = new Date("2024-10-18T23:30:00.000Z");
+    jest.setSystemTime(now);
+    const interval = 60;
+    const desiredTime = "01:00";
+    const result = calculateFutureDate(interval, desiredTime, true);
+    expect(result.toISOString()).toEqual("2024-10-18T23:30:00.000Z");
+  });
+
+  test("Current time late night and desired time next morning with long interval", () => {
+    const now = new Date("2024-10-18T23:30:00.000Z");
+    jest.setSystemTime(now);
+    const interval = 24 * 60 * 60; // 7 day
+    const desiredTime = "01:00";
+    const result = calculateFutureDate(interval, desiredTime, true);
+    expect(result.toISOString()).toEqual("2024-10-18T01:00:00.000Z");
+  });
 });
