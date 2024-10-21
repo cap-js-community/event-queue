0d44635d10c3cc4238a46a177439cba51f2addbe
Max Gruenfelder <max.grunfelder@gmail.com>
2024-10-21 15:53:52 +0200

    fix cds build (#238)
    Co-authored-by: Max Gruenfelder <maximilian.gruenfelder@sap.com>

--- cds-plugin.js
+++ cds-plugin.js
@@ -1,10 +1,22 @@
 "use strict";
 
 const cds = require("@sap/cds");
+const cdsPackage = require("@sap/cds/package.json");
 
 const eventQueue = require("./src");
 const COMPONENT_NAME = "/eventQueue/plugin";
+const SERVE_COMMAND = "serve";
 
-if (!cds.build?.register && Object.keys(cds.env.eventQueue ?? {}).length) {
+const isServe = cds.cli?.command === SERVE_COMMAND;
+const isBuild = cds.build?.register;
+// NOTE: for sap/cds < 8.2.3 there was no consistent way to detect cds is running as a server, not for build, compile,
+//   etc...
+const doLegacyBuildDetection =
+  cdsPackage.version.localeCompare("8.2.3", undefined, { numeric: true, sensitivity: "base" }) < 0;
+if ((doLegacyBuildDetection && isBuild) || (!doLegacyBuildDetection && !isServe)) {
+  return;
+}
+
+if (Object.keys(cds.env.eventQueue ?? {}).length) {
   module.exports = eventQueue.initialize().catch((err) => cds.log(COMPONENT_NAME).error(err));
 }
