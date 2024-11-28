b5090c558c267161fc10956cbd9cb64965b8a3e1
Max Gruenfelder <maximilian.gruenfelder@sap.com>
2024-11-28 10:11:53 +0100

    avoid issues by event-loop

--- src/shared/SetIntervalDriftSafe.js
+++ src/shared/SetIntervalDriftSafe.js
@@ -2,8 +2,6 @@
 
 const COMPONENT = "eventQueue/SetIntervalDriftSafe";
 
-const ALLOWED_SHIFT_IN_PROCENT = 0.1;
-
 class SetIntervalDriftSafe {
   #adjustedInterval;
   #interval;
@@ -21,12 +19,6 @@ class SetIntervalDriftSafe {
     const now = Date.now();
     if (this.#expectedCycleTime === 0) {
       this.#expectedCycleTime = now + this.#interval;
-    } else if (
-      Math.abs(now + this.#interval - this.#nextTickScheduledFor - this.#interval) >
-      this.#interval * ALLOWED_SHIFT_IN_PROCENT
-    ) {
-      this.#logger.log("overlapping ticks, skipping this run");
-      return;
     } else {
       this.#adjustedInterval = this.#interval - (now - this.#expectedCycleTime);
       this.#expectedCycleTime += this.#interval;
