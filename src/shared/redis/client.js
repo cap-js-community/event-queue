e1b4208c67a81cfa533531cadcca67be5ecf50ee
Max Gruenfelder <maximilian.gruenfelder@sap.com>
2025-11-05 11:37:25 +0100

    fixes

--- src/shared/redis/client.js
+++ src/shared/redis/client.js
@@ -2,44 +2,38 @@
 
 const { RedisClient } = require("@cap-js-community/common");
 
-const { getEnvInstance } = require("../env");
 const config = require("../../config");
 
 const REDIS_CLIENT_NAME = "eventQueue";
 
 const createMainClientAndConnect = async () => {
-  // TODO: why websockets: options?.config
   const redisClient = RedisClient.default(REDIS_CLIENT_NAME);
   return await redisClient.createMainClientAndConnect(config.redisOptions);
 };
 
 const subscribeRedisChannel = async (channel, subscribeHandler) => {
-  // TODO: why websockets: options?.config
   const redisClient = RedisClient.default(REDIS_CLIENT_NAME);
   const channelWithNamespace = [config.redisNamespace, channel].join("_");
   return await redisClient.subscribeChannel(config.redisOptions, channelWithNamespace, subscribeHandler);
 };
 
 const publishMessage = async (channel, message) => {
-  // TODO: why websockets: options?.config
   const redisClient = RedisClient.default(REDIS_CLIENT_NAME);
   const channelWithNamespace = [config.redisNamespace, channel].join("_");
   return await redisClient.publishMessage(config.redisOptions, channelWithNamespace, message);
 };
 
 const connectionCheck = async () => {
-  // TODO: why websockets: options?.config
   const redisClient = RedisClient.default(REDIS_CLIENT_NAME);
   return await redisClient.connectionCheck(config.redisOptions);
 };
 
 const isClusterMode = () => {
-  if (!("__clusterMode" in isClusterMode)) {
-    const env = getEnvInstance();
-    const { credentials } = env.redisRequires;
-    isClusterMode.__clusterMode = credentials.cluster_mode;
-  }
-  return isClusterMode.__clusterMode;
+  return RedisClient.default(REDIS_CLIENT_NAME).isCluster;
+};
+
+const registerShutdownHandler = (cb) => {
+  RedisClient.default(REDIS_CLIENT_NAME).beforeCloseHandler = cb;
 };
 
 module.exports = {
@@ -48,4 +42,5 @@ module.exports = {
   publishMessage,
   connectionCheck,
   isClusterMode,
+  registerShutdownHandler,
 };
