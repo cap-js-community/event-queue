940fa1ca040a90fee9b631c96b643396e73183ac
Max Gruenfelder <maximilian.gruenfelder@sap.com>
2025-11-05 11:46:04 +0100

    fixes

--- src/shared/redis/client.js
+++ src/shared/redis/client.js
@@ -7,33 +7,33 @@ const config = require("../../config");
 const REDIS_CLIENT_NAME = "eventQueue";
 
 const createMainClientAndConnect = async () => {
-  const redisClient = RedisClient.default(REDIS_CLIENT_NAME);
+  const redisClient = RedisClient.create(REDIS_CLIENT_NAME);
   return await redisClient.createMainClientAndConnect(config.redisOptions);
 };
 
 const subscribeRedisChannel = async (channel, subscribeHandler) => {
-  const redisClient = RedisClient.default(REDIS_CLIENT_NAME);
+  const redisClient = RedisClient.create(REDIS_CLIENT_NAME);
   const channelWithNamespace = [config.redisNamespace, channel].join("_");
   return await redisClient.subscribeChannel(config.redisOptions, channelWithNamespace, subscribeHandler);
 };
 
 const publishMessage = async (channel, message) => {
-  const redisClient = RedisClient.default(REDIS_CLIENT_NAME);
+  const redisClient = RedisClient.create(REDIS_CLIENT_NAME);
   const channelWithNamespace = [config.redisNamespace, channel].join("_");
   return await redisClient.publishMessage(config.redisOptions, channelWithNamespace, message);
 };
 
 const connectionCheck = async () => {
-  const redisClient = RedisClient.default(REDIS_CLIENT_NAME);
+  const redisClient = RedisClient.create(REDIS_CLIENT_NAME);
   return await redisClient.connectionCheck(config.redisOptions);
 };
 
 const isClusterMode = () => {
-  return RedisClient.default(REDIS_CLIENT_NAME).isCluster;
+  return RedisClient.create(REDIS_CLIENT_NAME).isCluster;
 };
 
 const registerShutdownHandler = (cb) => {
-  RedisClient.default(REDIS_CLIENT_NAME).beforeCloseHandler = cb;
+  RedisClient.create(REDIS_CLIENT_NAME).beforeCloseHandler = cb;
 };
 
 module.exports = {
